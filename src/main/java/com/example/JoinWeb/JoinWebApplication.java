30c007253ab52e776b9a1273813602bd96583efe
hyeyeongAn <mybymine64@naver.com>
2025-07-17 18:20:49 +0900

    hello and write YAML

---
+++ src/main/java/com/example/JoinWeb/JoinWebApplication.java
@@ -0,0 +1,14 @@
+package com.example.JoinWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinWebApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/example/JoinWeb/JoinWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JoinWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
